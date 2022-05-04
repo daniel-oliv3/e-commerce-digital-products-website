6a4670e30838b8e5c1bc0184ae9c0438ec63c31f
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-05-04 16:19:09 -0300

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Digital Store</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="img/favicon.ico"/>
+</head>
+<body>
+
+	<?php
+
+		echo "Olá mundo! Sapup3 na Área..."
+	
+	?>
+
+	
+	<script src="js/script.js"></script>
+</body>
+</html>
+
+<!--
+    Autor: Daniel Oliveira
+    Email: danieloliveira.webmaster@gmail.com
+    Manaus/Amazonas
+    04/05/2022
+-->
