b17424825dffd3edf662a19ff2dbf759ae3fb2c6
Daniel Oliveira <danielsapup3@gmail.com>
2022-12-28 11:50:18 -0300

    Update Login form

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -10,10 +10,30 @@ include '../components/connect.php';
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Login</title>
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="../css/admin_style.css">
 	<link rel="shortcut icon" href="img/favicon.ico"/>
+	<!-- Font Awesome CDN Link -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 </head>
 <body>
+<!-- ======= Seção do formulário de login do administrador =======  -->
+<section class="form-container">
+	<form action="" method="POST">
+		<h3>Conecte-se agora</h3>
+		<p>nome de usuário padrão = <span>admin</span> & senha = <span>111</span></p>
+		<input type="text" name="nome" maxlength="25" required placeholder="Digite seu nome" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+		<input type="password" name="senha" maxlength="25" required placeholder="Digite sua senha" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+		<input type="submit" class="btn" name="submit" value="Conecte-se agora">
+	</form>
+</section>
+
+
+
+
+
+
+
+
 
 
 
