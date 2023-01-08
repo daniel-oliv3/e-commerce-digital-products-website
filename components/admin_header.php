3008b28f3a03bbe8d2c66e72f6f4eec3d6e43a61
Daniel Oliveira <danielsapup3@gmail.com>
2023-01-08 08:05:53 -0400

    Update

--- components/admin_header.php
+++ components/admin_header.php
@@ -1,29 +1,31 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Digital Store</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="shortcut icon" href="img/favicon.ico"/>
-</head>
-<body>
-
-	<?php
-
-		echo "Olá mundo! Sapup3 na Área..."
-	
-	?>
-
-	
-	<script src="js/script.js"></script>
-</body>
-</html>
+<header class="header">
+	<section class="flex">
+		<a href="dashboard.php" class="logo">Administrador<span>Painel</span></a>
+		<nav class="navbar">
+			<a href="dashboard.php">Inicio</a>
+			<a href="products.php">Produtos</a>
+			<a href="placed_orders.php">Pedidos feitos</a>
+			<a href="admin_accounts.php">Contas de administrador</a>
+			<a href="user_accounts.php">Contas de usuário</a>
+			<a href="messages.php">Mensagens</a>
+		</nav>
+		<div class="icons">
+			<div id="menu-btn" class="fas fa-bars"></div>
+			<div id="user-btn" class="fas fa-bars"></div>
+		</div>
+		<div class="profile">
+			<?php
+			$select_profile = $conn->prepare("SELECT * FROM `admins` WHERE id = ?");
+			$select_profile->execute([$admin_id]);
+			$fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);			
+			?>
+		</div>
+	</section>
+</header>
 
 <!--
     Autor: Daniel Oliveira
     Email: danieloliveira.webmaster@gmail.com
     Manaus/Amazonas
-    04/05/2022
+    08/01/2023
 -->
